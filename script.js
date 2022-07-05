4c712deb5c26f2e2bf8d7c6027afd72f634678a4
Linh Vu <linh777gkog@gmail.com>
2022-07-05 23:39:57 +0700

    add dbclick

--- script.js
+++ script.js
@@ -9,16 +9,33 @@ getNotes().forEach((note) => {
 addNoteButton.addEventListener('click', () => addNote());
 
 function getNotes() {
-  return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
+  return JSON.parse(localStorage.getItem('stickynotes-notes') || '[]');
 }
 
 function saveNotes(notes) {
-  localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
+  localStorage.setItem('stickynotes-notes', JSON.stringify(notes));
 }
 
 function createNoteElement(id, content) {
-  const element = document.createElement("textarea");
+  const element = document.createElement('textarea');
 
-  element.classList.add("note");
+  element.classList.add('note');
   element.value = content;
-  element.placeholder = "Empty Sticky Note";
+  element.placeholder = 'Empty Sticky Note';
+
+  element.addEventListener('change', () => {
+    updateNote(id, element.value);
+  });
+
+  element.addEventListener('dblclick', () => {
+    const doDelete = confirm(
+      'Are you sure you wish to delete this sticky note?'
+    );
+
+    if (doDelete) {
+      deleteNote(id, element);
+    }
+  });
+
+  return element;
+}
