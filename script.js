6531e67013ffaf8d4e1533a06d3ea5d8b4174010
Linh Vu <linh777gkog@gmail.com>
2022-07-05 23:38:57 +0700

    check point

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const notesContainer = document.getElementById('app');
 const addNoteButton = notesContainer.querySelector('.add-note');
+
+getNotes().forEach((note) => {
+  const noteElement = createNoteElement(note.id, note.content);
+  notesContainer.insertBefore(noteElement, addNoteButton);
+});
+
+addNoteButton.addEventListener('click', () => addNote());
