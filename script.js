409091fb70650fe6b757d31c31c4c4d041224ac9
Linh Vu <linh777gkog@gmail.com>
2022-07-05 23:39:25 +0700

    save notes

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ getNotes().forEach((note) => {
 });
 
 addNoteButton.addEventListener('click', () => addNote());
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
+}
+
+function saveNotes(notes) {
+  localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
+}
+
+function createNoteElement(id, content) {
+  const element = document.createElement("textarea");
+
+  element.classList.add("note");
+  element.value = content;
+  element.placeholder = "Empty Sticky Note";
